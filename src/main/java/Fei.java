3a461d90e7d3172fec54244324291f5f2e4eb1a2
Yi Fei <e1281068@u.nus.edu>
2026-01-26 11:17:21 +0800

    Complete Level 0

---
+++ src/main/java/Fei.java
@@ -0,0 +1,18 @@
+public class Fei {
+
+    public void sayHi(){
+        System.out.println("Hello! I'm Fei\nWhat can I do for you?");
+    }
+
+    public void sayBye(){
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    public static void main(String[] args) {
+        Fei fei = new Fei();
+        fei.sayHi();
+        // while (true){
+            
+        // }
+        fei.sayBye();
+    }
+}
